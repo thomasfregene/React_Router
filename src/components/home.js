c7a7e3063c80c9550e226aa4a104a58e4a5782da
thomasfregene <fregzythomas@gmail.com>
2020-08-06 23:01:57 +0100

    Lifecycle Hooks on Creating a Component

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 
 class Home extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {name: 'francis'}
+        console.log('1- constructor')
+    }
+    static getDerivedStateFromProps(){
+        console.log('2-getDerivedStateFromProps')
+        return null
+    }
+
     render(){
+        console.log('3-render')
         return(
             <div>Home</div>
         ) 
+    }
+
+    componentDidMount(){
+        console.log('4-componentDidMount')
+    }
+
+    componentWillUnmount(){
+        console.log('x-componentDidMount')
     }
    
 }
