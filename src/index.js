2f4f75892c2cae295c69e408ba30e64d0c537374
thomasfregene <fregzythomas@gmail.com>
2020-08-06 13:32:52 +0100

    Creating Routes -index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ import Posts from './components/post';
 import Profile from './components/profile';
 
 const App =()=>(
-  <div>
-    Hello
-  </div>
+
+<BrowserRouter>
+ <header>
+    <div>header</div>
+  </header>
+  <Route path='/' exact component={Home}/>
+  <Route path='/posts' component={Posts}/>
+  <Route path='/profile' component={Profile}/>
+ </BrowserRouter>
 )
 ReactDOM.render(<App />, document.getElementById('root'));
 
