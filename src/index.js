98c038269cb1fc94d7cf3f1c54429677153d3000
thomasfregene <fregzythomas@gmail.com>
2020-08-06 18:41:04 +0100

    Using Params to fetch Items -PostItem Component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 import Home from './components/home';
 import Posts from './components/post';
 import Profile from './components/profile';
+import PostItem from './components/post_item';
 
 const App =()=>(
 
@@ -20,8 +21,9 @@ const App =()=>(
     <br/><br/>
   </header>
   <Route path='/' exact component={Home}/>
-  <Route path='/posts' component={Posts}/>
-  <Route path='/profile' component={Profile}/>
+  <Route path='/posts' exact component={Posts}/>
+  <Route path='/posts/:id' component={PostItem}/>
+  <Route path='/profile' exact component={Profile}/>
  </BrowserRouter>
 )
 ReactDOM.render(<App />, document.getElementById('root'));
