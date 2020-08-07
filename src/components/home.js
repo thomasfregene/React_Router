03044047f80971c48078f9c45541cd4ecd993b42
thomasfregene <fregzythomas@gmail.com>
2020-08-07 02:43:41 +0100

    Lifecycle hooks for when updating component -home.js

--- src/components/home.js
+++ src/components/home.js
@@ -7,16 +7,41 @@ class Home extends Component{
         this.state = {name: 'francis'}
         console.log('1- constructor')
     }
-    static getDerivedStateFromProps(){
+    static getDerivedStateFromProps(props, state){
+        if(state.name === 'Ron'){
+            return{
+                name: 'Milehouse'
+            }
+        }
         console.log('2-getDerivedStateFromProps')
         return null
     }
 
+    shouldComponentUpdate(nextProps, nextState){
+        //console.log('x- shouldComponentUpdate')
+       if(nextState.name === 'Steve'){
+           return false;
+       }
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+       let age = 20;
+        return age;
+    }
+
     render(){
-        console.log('3-render')
+        console.log(this.state)
         return(
-            <div>Home</div>
+            <div onClick={()=> this.setState({name: 'Ron'})}>Change Name</div>
         ) 
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('x- componentDidUpdate')
+        // console.log(prevState)
+        // console.log(this.state)
+        console.log(snapshot)
     }
 
     componentDidMount(){
